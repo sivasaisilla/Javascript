5309898bd5fcfa44a709c710a8cc408d95b2d09f
sivasai <sivasai3920@gmail.com>
2023-02-03 23:25:58 +0530

    Learned about IIFE, Closures

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -52,16 +52,93 @@ const book = lufthansa.book;
 //   .querySelector('.buy')
 //   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
 
+/////////////////////
 //Partial Function
+////////////////////
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
 
-const addTax = (rate, value) => value + value * rate;
-console.log(addTax(0.1, 200));
-
-const addVAT = addTax.bind(null, 0.23);
-console.log(addVAT(100));
+// const addVAT = addTax.bind(null, 0.23);
+// console.log(addVAT(100));
 
 // //as no this keyword is mentioned here we used null
 // const addVAT = addTax.bind(null, 0.23);
 // console.log(addVAT(100));
 
+//
+//
+
+/////////////////////////
 ///CODING CHALLENGE 01;
+///////////////////////////
+// const poll = {
+//   question: 'What is your favourite programming language?',
+//   options: ['0: JavaScript', '1: Python', '2: Rust', '3:C++'],
+//   // This generates [0, 0, 0, 0]. More in the next section!
+//   answers: new Array(4).fill(0),
+//   registerNewAnswer() {
+//     const answer = Number(
+//       prompt(
+//         `${this.question}\n${this.options.join('\n')}\n(Write option number )`
+//       )
+//     );
+
+//     //register answer
+//     console.log(answer);
+//     typeof answer === 'number' &&
+//       answer < this.answers.length &&
+//       this.answers[answer]++;
+
+//     this.displayResults();
+//     this.displayResults('string');
+//   },
+
+//   displayResults(type = 'array') {
+//     if (type === 'array') {
+//       console.log(this.answers);
+//     } else if (type === 'string') {
+//       console.log(`Poll results are ${this.answers.join(', ')}`);
+//     }
+//   },
+// };
+
+// // poll.registerNewAnswer();
+
+// document
+//   .querySelector('.poll')
+//   .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+//
+//
+//
+
+//IIFE
+// (function () {
+//   console.log('This will never run again');
+// });
+
+//
+//
+//
+
+///////////////////
+//CLOSURES
+//////////////////
+// let f;
+
+// const g = function () {
+//   const a = 23;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
+
+// const h = function () {
+//   const b = 777;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
+
+// g();
+// f();
