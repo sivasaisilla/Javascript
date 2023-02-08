8400aa7eccf8d8f3aeb465c30043fee4dc9f9ee5
sivasai <sivasai3920@gmail.com>
2023-02-08 18:36:21 +0530

    learned about filter method

--- 11-Arrays/script.js
+++ 11-Arrays/script.js
@@ -184,3 +184,18 @@ displayMovements(account1.movements);
 
 // createUsernames(accounts);
 // console.log(accounts);
+
+/////////////////////////
+//Filter method
+/////////////////////////
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+
+const withdrawals = movements.filter(function (mov) {
+  return mov < 0;
+});
+console.log(movements);
+console.log(deposits);
+console.log(withdrawals);
