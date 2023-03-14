e3cf2db0b48c4ea21a3260988a19ba1922d29c7b
sivasai <sivasai3920@gmail.com>
2023-03-14 19:43:10 +0530

    Loan button & learned abt Some and Every methods

--- 11-Arrays/script.js
+++ 11-Arrays/script.js
@@ -190,6 +190,21 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1))
+    //Add movement
+    currentAccount.movements.push(amount);
+
+  //Update Ui
+  updateUI(currentAccount);
+
+  inputLoanAmount.value = '';
+});
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
 
