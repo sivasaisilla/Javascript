06381a1be20fe0a86232f308f8d3b3624de91dd2
Sivasai <sivasai3920@gmail.com>
2023-06-21 11:53:16 +0530

    Implementing fadeout in navigation

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -2,7 +2,6 @@
 
 ///////////////////////////////////////
 
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
@@ -10,6 +9,12 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 // Modal window
 const openModal = function (e) {
@@ -54,7 +59,6 @@ btnScrollTo.addEventListener('click', function (e) {
   });
 });
 
-
 //////////////////////
 //PAGE NAVIGATION
 //////////////////////
@@ -75,16 +79,15 @@ btnScrollTo.addEventListener('click', function (e) {
 //1. Add event lsitener to common parent element
 // 2. Determine what element originated the event
 
-document.querySelector('.nav__links').addEventListener('click', function(e){
+document.querySelector('.nav__links').addEventListener('click', function (e) {
   e.preventDefault();
-  
-  //matching strategy
-  if(e.target.classList.contains(".nav__link")){
-    const id = e.target.getAttribute('href')
-    document.querySelector(id).scrollIntoView({behavior:'smooth'})
-  }
-})
 
+  //matching strategy
+  if (e.target.classList.contains('.nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
 
 /////
 // Bubbling Concept
@@ -112,3 +115,50 @@ document.querySelector('.nav__links').addEventListener('click', function(e){
 //   this.style.backgroundColor = randomColor();
 //   console.log('LINK', e.target, e.currentTarget);
 // });
+
+//
+// TABBED COMPONENT
+//
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard Clause
+  if (!clicked) return;
+
+  //remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  //activate tab
+  clicked.classList.add('operations__tab--active');
+
+  //activate content area
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+//Menu fade animation
+
+const handleHover = function (e, opacity) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = opacity;
+    });
+    logo.style.opacity = opacity;
+  }
+};
+
+nav.addEventListener('mouseover', function (e) {
+  handleHover(e, 0.5);
+});
+
+nav.addEventListener('mouseout', function (e) {
+  handleHover(e, 1);
+});
